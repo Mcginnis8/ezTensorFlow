4fdaf517ab56198e5d1e50418633da6bb925804e
Cole McGinnis <camvols19@gmail.com>
2023-01-12 16:50:13 -0500

    Final almost ready to push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ezTensorFlow
+A package to easily make a tensorflow model (for beginners in ML)
--- code_dir/testCreateModel.py
+++
@@ -1,4 +0,0 @@
-import tensorflow  
-import createModel as ezTensor
-
-testModel = ezTensor.createModel()
